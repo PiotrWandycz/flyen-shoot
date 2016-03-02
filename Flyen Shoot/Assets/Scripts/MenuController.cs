26f34e416c3ddd5115eb01757424e2b97d82e899
Piotr Wandycz <Piotr Wandycz>
2016-03-02 22:13:52 +0100

    Added MenuController, modified buttons

---
+++ Flyen Shoot/Assets/Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour 
+{
+    public string StartLevel;
+
+    public void StartGame()
+    {
+        Application.LoadLevel(StartLevel);
+    }
+    
+    public void OpenOptions()
+    {
+        Debug.Log("Options clicked.");
+    }
+
+    public void OpenHighscore()
+    {
+        Debug.Log("Highscore clicked.");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Finito!");
+        Application.Quit();
+    }
+}
