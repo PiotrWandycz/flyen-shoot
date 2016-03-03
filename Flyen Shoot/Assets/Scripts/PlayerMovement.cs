2bd21d886d9a04595b44f96b790d3d1918bfb0d9
Piotr Wandycz <Piotr Wandycz>
2016-03-03 21:03:24 +0100

    Addded player

---
+++ Flyen Shoot/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 5.0f;
+    private Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+        BoundPlayer();
+    } 
+
+    void MovePlayer()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        rigidbody.velocity = movement * Speed;
+    }
+
+    void BoundPlayer()
+    {
+        // todo: zablokować wyjeżdzanie poza ekran
+    }
+}
