afb0665fb62dd9800fe575beac369841fd842cf8
Piotr Wandycz <Piotr Wandycz>
2016-03-14 23:02:30 +0100

    Added MapController.cs

---
+++ Flyen Shoot/Assets/Scripts/Controllers/MapController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MapController : MonoBehaviour 
+{
+    public float SliderMaxValue = 60.0f;
+    public float SliderStartValue = 0.0f;
+    public Slider MapLengthSlider;
+    public string NextLevel;
+
+	void FixedUpdate () 
+    {
+        SliderStartValue += Time.deltaTime;
+        MapLengthSlider.value = SliderStartValue;
+
+        if (SliderStartValue > SliderMaxValue)
+        {
+            Application.LoadLevel(NextLevel);
+        }
+	}
+}
