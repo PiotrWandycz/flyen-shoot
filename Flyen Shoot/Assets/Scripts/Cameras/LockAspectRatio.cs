cbeb4d616c9af3446c77a0b26389b9b34b383429
Piotr Wandycz <Piotr Wandycz>
2016-04-18 20:22:42 +0200

    Backup (format)

---
+++ Flyen Shoot/Assets/Scripts/Cameras/LockAspectRatio.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockAspectRatio : MonoBehaviour
+{
+    void Start()
+    {
+        float targetAspect = 4.0f / 3.0f;
+
+        float windowAspect = (float)Screen.width / (float)Screen.height;
+
+        // current viewport height should be scaled by this amount
+        float scaleHeight = windowAspect / targetAspect;
+
+        // obtain camera component so we can modify its viewport
+        Camera camera = GetComponent<Camera>();
+
+        // if scaled height is less than current height, add letterbox
+        if (scaleHeight < 1.0f)
+        {
+            Rect rect = camera.rect;
+
+            rect.width = 1.0f;
+            rect.height = scaleHeight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
+
+            camera.rect = rect;
+        }
+        else // add pillarbox
+        {
+            float scalewidth = 1.0f / scaleHeight;
+
+            Rect rect = camera.rect;
+
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+    }
+}
